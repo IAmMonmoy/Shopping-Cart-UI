37184e51d50649d41777ade25dbe67a2e9e2f82d
IAmMonmoy <iammonmoy@gmail.com>
2018-04-18 20:34:59 +0600

    added services needed for authentication

---
+++ src/app/admin/services/role-guard.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+
+@Injectable()
+export class RoleGuardService {
+
+  constructor() { }
+
+}
