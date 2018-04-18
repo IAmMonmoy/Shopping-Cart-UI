32e925e21a3dc316967bd24600a3191b14ba91d5
IAmMonmoy <iammonmoy@gmail.com>
2018-04-18 20:46:00 +0600

    implemented auth guard for expired token

--- src/app/admin/services/auth.service.ts
+++ src/app/admin/services/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(public jwtHelper: JwtHelperService) { }
 
+  public isAuthenticated(): boolean 
+  {
+      const token = localStorage.getItem('token');
+
+      return !this.jwtHelper.isTokenExpired(token);
+  }
 }
