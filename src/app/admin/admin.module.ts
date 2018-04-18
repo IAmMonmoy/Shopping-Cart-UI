999279cd9a94026631e00c8022c6aefbad401740
IAmMonmoy <iammonmoy@gmail.com>
2018-04-18 10:53:15 +0600

    added adminModule to app component and added authenticate component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AddProductComponent]
+  declarations: [AddProductComponent, AuthenticateComponent]
 })
 export class AdminModule { }
