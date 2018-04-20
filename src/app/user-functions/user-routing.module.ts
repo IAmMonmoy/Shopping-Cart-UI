9c5bb70a5bcd5c0b6055054b5be919512358185f
IAmMonmoy <iammonmoy@gmail.com>
2018-04-20 22:29:15 +0600

    added necessary routing

--- src/app/user-functions/user-routing.module.ts
+++ src/app/user-functions/user-routing.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { Product } from '../shared/AllModels';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+const routes : Routes = [
+  { path: 'allProduct', component: AllProductsComponent },
+  { path: 'product/:id', component: Product },
+  { path: 'cart', component: ShoppingCartComponent }
+]
 
 @NgModule({
   imports: [
@@ -7,4 +17,6 @@ import { CommonModule } from '@angular/common';
   ],
   declarations: []
 })
+
+
 export class UserRoutingModule { }
